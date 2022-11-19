757b4b2cc5e911519f267ad07d681e772053cc88
Flotss <80858668+Flotss@users.noreply.github.com>
2022-11-19 15:11:01 +0100

    Exit of program, and colors

--- UpdateAllPrograms.bat
+++ UpdateAllPrograms.bat
@@ -1,36 +1,32 @@
-@echo off
+@ECHO off
 
 @title Update your programs
 SetLocal EnableDelayedExpansion
 Setlocal EnableExtensions 
 
 :debut
-WINGET UPGRADE
+WINGET UPGRADE --include-unknown
 
 
-
-:while
-ECHO Voulez-vous mettre a jour tous vos programmes ?
+:MiseAjour
+ECHO Voulez-vous mettre a jour tous vos programmes ?
 set /p YNUpdate=(Yes/No): 
 
 
 IF !YNUpdate! == Yes (
-	ECHO "Je me mets a jour"
-	WINGET UPGRADE --ALL
+	call :affichageCouleurVert "Mise a jour des programmes..."
+	WINGET UPGRADE --ALL --include-unknown
+	ECHO.
+	call :affichageCouleurBleu "Mise a jour des programmes terminee"
 ) else (
 	IF !YNUpdate! == No (
-		ECHO "Aucune mise a jour a ete faite"
+		call :affichageCouleurVert "Mise a jour des programmes annulee"
+		goto :exit
 	) else (
-		echo "Saisie incorrecte veuillez ressaisir votre reponse"
-		goto :while
+		call :affichageCouleurRouge "Saisie incorrecte veuillez ressaisir votre reponse"
+		ECHO.
+		goto :MiseAjour
 	)
-)
-
-
-IF !YNUpdate! == No (
-	echo Done.
-	timeout 5
-	exit /b
 )
 
 :verif
@@ -38,15 +34,34 @@ ECHO Voulez-vous faire une verification ?
 set /p YNVERIF=(Yes/No): 
 
 IF !YNVERIF! == Yes (
-	ECHO Verification en cours
+	call :affichageCouleurVert "Verification en cours..."
 	goto :debut
 ) else (
 	IF !YNVERIF! == No (
-		ECHO Done.
-		timeout 5
-		exit /b
+		call :affichageCouleurVert "Verification annulee"
+		goto :exit
 	) else (
-		echo "Saisie incorrecte veuillez ressaisir votre reponse"
+		call :affichageCouleurRouge "Saisie incorrecte veuillez ressaisir votre reponse"
+		ECHO.
 		goto :verif
 	)
 )
+
+:exit
+ECHO Done.
+timeout 5
+exit /b
+
+
+
+:affichageCouleurVert
+ECHO [32m%~1 [97m
+EXIT /B 0
+
+:affichageCouleurRouge
+ECHO [31m%~1 [97m
+EXIT /B 0
+
+:affichageCouleurBleu
+ECHO [94m%~1 [97m
+EXIT /B 0
